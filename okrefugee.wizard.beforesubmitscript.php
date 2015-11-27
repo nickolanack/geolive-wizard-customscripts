3625dcebf77016f94f53ad5a252f7a202072bef1
nickolanack <nickblackwell82@gmail.com>
2015-11-27 13:52:42 -0800

    add custom privacy selection

--- okrefugee.wizard.beforesubmitscript.php
+++ okrefugee.wizard.beforesubmitscript.php
@@ -19,3 +19,62 @@ switch(layer.getName()){
 }
 
 </script>
+
+
+<script type="text/javascript">
+
+/**
+ * Custom visibility selection for Volunteer Opportunities
+ */
+
+<?php 
+
+	IncludeCSSBlock('
+
+		.tags-publish span{}
+		.tags-publish span.selected{
+			background-color: cornflowerblue;
+		}
+
+		');
+?>
+
+
+var published="everyone";
+var unpublished="just coordinators, and me";
+
+var defaultValue=published;
+
+var setPublishedTo=function(v){
+	if(v==published){
+		wizardDataSet.readAccess="public"
+	}else{
+		wizardDataSet.readAccess="special"
+	}
+};
+
+return (new TagCloudSelectionModule(application, item, {
+		values:[defaultValue],
+		tags:[unpublished, published],
+		shuffle:false,
+        maxSelected:1,
+        label:'Who can see this item',
+        className:'tags-publish'
+	})).addEvent('change',function(selection){
+
+		var cloud=this;
+
+		if(selection.length==0){
+			if(wizardDataSet.readAccess=="public"){
+				cloud.addValue(unpublished);
+			}else{
+				cloud.addValue(published)
+			}
+		}else{
+			setPublishedTo(selection[0]);
+		}
+
+
+	});
+
+</script>
